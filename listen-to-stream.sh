d06a403e4c30dff6e19e7f066c00eddeba338a71
Henrik Johnsen <29645801+Am0rphous@users.noreply.github.com>
2020-07-03 16:37:29 +0200

    Create listen-to-stream.sh

---
+++ listen-to-stream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Windows: If you are in same folder as the ffplay.exe file:
+.\ffplay.exe rtp://server:port
+.\ffplay.exe udp://192.168.1.113:1234
+
+
+#Linux
+ffplay.exe rtp://ip:port
